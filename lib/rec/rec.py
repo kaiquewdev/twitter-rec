d75656526e9974394188e2902cfcc399abf5215f
Kaique da Silva <kaique.developer@gmail.com>
2012-11-17 18:20:49 -0200

    Rec new structure

---
+++ lib/rec/rec.py
@@ -0,0 +1,9 @@
+from filter import Filter
+from text import Text
+from stack import Stack 
+
+class Rec( object ):
+    def __init__(self):
+        self.filter = Filter()
+        self.text = Text()
+        self.stack = Stack()
