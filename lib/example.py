618f92b02e2a86674b63587987db2ad9c190cd59
Kaique da Silva <kaique.developer@gmail.com>
2012-11-17 22:11:35 -0200

    Example
    refactor

--- lib/example.py
+++ lib/example.py
@@ -1,36 +1,37 @@
+#!/usr/bin/env python
 # coding: utf-8
-# Example of rec
 
+# Example of rec
 import sys
 import requests
-import StringIO
 
-from rec import Rec
 from string import join
+from rec import Rec
 
 def main():
     rec = Rec()
 
-    favorites_url = 'https://api.twitter.com/1/favorites.json?count=10&screen_name={0}'.format( sys.argv[1] )
-    favorites = requests.get( favorites_url )
+    twitter = {
+        'favorites': 'https://api.twitter.com/1/favorites.json?count=20&screen_name={0}'.format( sys.argv[1] ),
+        'search': 'http://search.twitter.com/search.json?q={0}&rpp=2&include_entities=true&result_type=mixed'.format( sys.argv[2] )
+    }
 
-    fav_list = [tw['text'] for tw in favorites.json]
-    fav_text = join( fav_list, '\n' )
+    favorites = requests.get( twitter['favorites'] )
+    search = requests.get( twitter['search'] )
 
-    fav_buffer = StringIO.StringIO()
-    fav_buffer.write( fav_text )
+    fav_list = [ tw['text'] for tw in favorites.json ]
+    search_list = [ tw['text'] for tw in search.json['results'] ][:2]
 
+    fav_buffer = rec.stack.buffer( fav_list ); 
     fav_frequency = rec.filter.frequency( fav_buffer.getvalue() )
-
-    search_url = 'http://search.twitter.com/search.json?q={0}&rpp=2&include_entities=true&result_type=mixed'.format( sys.argv[2] )
-    search = requests.get( search_url )
-    search_list = [ tw['text'] for tw in search.json['results'] ]
-
-    feed_list = search_list[:2]
-    
     fav_buffer.close()
+
+    result = rec.stack.choose(
+        fav_frequency,
+        search_list
+    )
     
-    print rec.stack.choose( fav_frequency, feed_list )
+    print result 
 
 if __name__ == '__main__':
     main()
